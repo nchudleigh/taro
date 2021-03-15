4836d77c0e6d4a0cf0bb64b8db14ece4bbef4a7f
Neil <nchudleigh@gmail.com>
2021-03-15 00:10:29 -0400

    Delete unused files

--- taro/domain/router.ts
+++
@@ -1,9 +0,0 @@
-export interface Message {
-  kind: string;
-}
-
-export function isOfKind<T>(messageKind: string) {
-  return (message: any): message is T => {
-    return message?.kind === messageKind;
-  };
-}
