34ca9eca5f3ac17e4904e57c6fa0a10038096556
Neil <nchudleigh@gmail.com>
2021-03-15 00:35:28 -0400

    Create sendMessage server helper

--- taro/server/server.ts
+++ taro/server/server.ts
@@ -4,27 +4,43 @@ import { handleHTTP } from "@taro/server/fileserver.ts";
 import {
   handleSocket,
   isWebSocketRequest,
-  RawServerMessageHandler,
+  ServerMessageHandler,
+  WebSocket,
 } from "@taro/server/websocket.ts";
+
+import { Message } from "@taro/domain/domain.ts";
+import { packMessage } from "@taro/server/message.ts";
+import { isNull } from "@taro/utils.ts";
 
 type serverOptions = {
   assetDirectory: string;
   port: number;
 };
 
+export type { WebSocket };
+
 export async function createServer(
   options: serverOptions,
-  rawServerMessageHandler: RawServerMessageHandler,
+  serverMessageHandler: ServerMessageHandler,
 ) {
   console.log(`🍠 Taro Server listening at :${options.port}`);
   for await (const req of serve(`:${options.port}`)) {
     // If the request is requesting to upgrade to websocket
     // handle the connection request
     if (isWebSocketRequest(req)) {
-      handleSocket(req, rawServerMessageHandler);
+      handleSocket(req, serverMessageHandler);
     } // Otherwise, serve static files
     else {
       handleHTTP(req, options.assetDirectory);
     }
   }
+}
+
+export function sendMessage(
+  webSocket: WebSocket,
+  message: Message,
+) {
+  const packagedMessage = packMessage(message);
+  if (isNull(packagedMessage)) return;
+  webSocket.send(packagedMessage);
 }
