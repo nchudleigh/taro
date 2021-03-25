126801da4342fd87bcf3a8eb7b0c2d3fc90d3a38
Neil <nchudleigh@gmail.com>
2021-03-24 23:29:44 -0400

    Add join room message broadcast

--- example/server.ts
+++ example/server.ts
@@ -1,17 +1,34 @@
 import { Message } from "@taro/domain/domain.ts";
-import { createServer, sendMessage, WebSocket } from "@taro/server/server.ts";
-import { isCreateRoomMessage } from "./domain.ts";
+import {
+  broadcastMessage,
+  createServer,
+  sendMessage,
+  startServer,
+  WebSocket,
+} from "@taro/server/server.ts";
+import {
+  isCreateRoomMessage,
+  isJoinRoomMessage,
+  makeCreateRoomMessage,
+  makeJoinRoomMessage,
+} from "./domain.ts";
 
 const server = createServer({
   port: 8080,
   assetDirectory: `${Deno.cwd()}/example/assets`,
-}, handleMessage);
+});
 
-// TODO: Build a router abstraction around this
-function handleMessage(webSocket: WebSocket, message: Message) {
+startServer(server, (webSocket: WebSocket, message: Message) => {
   if (isCreateRoomMessage(message)) {
     // call create room
     console.log(message);
-    sendMessage(webSocket, { kind: "created_room" });
+    makeCreateRoomMessage();
   }
-}
+  if (isJoinRoomMessage(message)) {
+    console.log(message);
+    // Find room, do logic to move person to room
+    // for each connection in the room
+    // webSocket.broadcast(message)
+    broadcastMessage(server, message);
+  }
+});
